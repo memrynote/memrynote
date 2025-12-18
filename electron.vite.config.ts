ae7ece4da0aaa8c9948da1bfc54a4b36cddea9b7
Kaan Karaca <kaan94karaca@gmail.com>
2025-12-18 19:27:51 +0300

    resolve nodejs version and drizzle target

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,10 +2,21 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [
+      externalizeDepsPlugin(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'src/main/database/drizzle/*',
+            dest: 'drizzle'
+          }
+        ]
+      })
+    ],
     build: {
       rollupOptions: {
         external: ['better-sqlite3']
